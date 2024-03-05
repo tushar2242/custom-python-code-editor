c590b714db9f32be2832b9fc451511328a5083c3
Alok Sahu <tusharsahu2427@gmail.com>
2024-03-06 02:08:25 +0530

    first update

---
+++ runapi.js
@@ -0,0 +1,33 @@
+const runCode = async () => {
+
+    const editor = monaco.editor.getModels()[0]; // Get the first editor instance
+    const code = editor.getValue();
+
+
+    const output = document.getElementById('output')
+    const error = document.getElementById('error')
+
+
+
+    try {
+        const response = await fetch('https://python-runner-1.onrender.com/execute', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ code: code })
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch');
+        }
+
+        const res = await response.json();
+        console.log(res)
+        output.innerHTML = res.data
+    } catch (error) {
+        console.error(error);
+
+        error.innerHtml = error
+    }
+};
